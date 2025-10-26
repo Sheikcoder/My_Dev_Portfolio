0df943f78cf0e0b73d82fe354a232c87c8e695af
Sheik008 <159415082+Sheik008@users.noreply.github.com>
2025-10-26 21:49:16 +0800

    Updated portfolio project files

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 // import Particle from "../Particle";
-import pdf from "../../Assets/Sheik_2Yrs_Exp_React_Node.pdf";
+import pdf from "../../Assets/sheikresume.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
